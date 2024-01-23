2d4120a958c26f512b6c15ce79d70b067cd64027
Seidol <sebastienmuhire1@outlook.com>
2024-01-23 10:53:57 +0200

    This program will assign a random signed number to the variable number each time it is executed. Complete the source code in order to print whether the number stored in the variable number is positive or negative.

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -2,6 +2,6 @@
 import random
 
 number = random.randint(-10, 10)
-s = ["negative", "zero", "positive"]
-r = "{} is {}".format(number, s[1 + (number > 0)] if number != 0 else s[1])
+s = "negative" if number < 0 else "zero" if number == 0 else "positive"
+r = "{} is {}".format(number, s)
 print(r)
