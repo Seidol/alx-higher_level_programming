38cf0ef2565ea8c14bb6aea8c9f96f27cb69313d
Seidol <sebastienmuhire1@outlook.com>
2024-02-13 12:38:10 +0200

    ASCII alphabet in lowercase

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 
-"""Print the ASCII alphabet in lowercase, not followed by a new line."""
-
-print("".join(chr(letter) for letter in range(97, 123)), end="")
+for letter in range(97, 123):
+    print("{}".format(chr(letter)), end="")
