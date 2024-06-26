a46e51c29ff90a232535bbc55f194bedd13ea3cc
Seidol <sebastienmuhire1@outlook.com>
2024-06-26 20:29:19 +0200

    initial

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    result = 0
+    if isinstance(roman_string) == str and roman_string is not None:
+        string = roman_string
+        strlen = len(string)
+        std = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        for i in range(strlen):
+            if (i > 0) and (std[string[i]] > std[string[i - 1]]):
+                result += int(std[string[i]]) - int(std[string[i - 1]]) * 2
+            else:
+                result += std[string[i]]
+    return result
